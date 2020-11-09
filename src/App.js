eaf3cd1b7fd9651ea1c74c80f2c81b1d091a4ed9
bugiedokpesi <bugie.dokpesi@capgemini.com>
2020-11-09 00:07:31 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+import { withAuthenticator } from '@aws-amplify/ui-react';
+Amplify.configure(awsExports);
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    )
+
+  }
+  
 }
 
-export default App;
+export default withAuthenticator(App, true);
